f8972bb354af5fedc660fcb3d21b48de1b002a4c
Teodoro Garcia III <tedtke514@gmail.com>
2020-06-17 22:58:16 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,6 @@
 # Build your say_hello method here
-def say_hello(name, language=ruby)
+def say_hello(name, language)
   puts "Hello #{name} #{language} Programmer!"
 end
+ 
+say_hello(Gabriela, Ruby)
